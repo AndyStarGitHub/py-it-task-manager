293187ae6d3a3686d1593775b85854904fad2c86
AndyStar <andy_star@ukr.net>
2025-04-16 08:32:29 +0300

    First edition of models

--- tasker/models.py
+++ tasker/models.py
@@ -20,6 +20,7 @@ class Worker(AbstractUser):
     class Meta:
         verbose_name = "worker"
         verbose_name_plural = "workers"
+        ordering = ["username"]
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
